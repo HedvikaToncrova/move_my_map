fc995ce1193bef6eaaf2d55fb6cc01ceca73f0a6
Eleanor <eleanor.haproff@marvelapp.com>
2018-10-17 11:54:58 +0100

    ES6ify

--- js/index.js
+++ js/index.js
@@ -1,13 +1,11 @@
 
-function setup() {
+async function setup() {
     const imageElement = document.getElementById('cat');
     const imageScaleFactor = 0.5;
     const outputStride = 16;
     const flipHorizontal = false;
 
-    posenet.load().then(function(net){
-        return net.estimateSinglePose(imageElement, imageScaleFactor, flipHorizontal, outputStride)
-    }).then(function(pose){
-        console.log(pose);
-    })
+    const model = await posenet.load();
+    const pose = await model.estimateSinglePose(imageElement, imageScaleFactor, flipHorizontal, outputStride);
+    console.log(pose)
 }
